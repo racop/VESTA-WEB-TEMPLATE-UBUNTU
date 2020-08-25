949810cb05bc5d779ebc049c914de75310f6b8bc
Rahul Ranjan <33937472+rahulcoolranjan@users.noreply.github.com>
2020-08-26 00:45:41 +0530

    Update PHP-74.sh

--- PHP7.4/PHP-74.sh
+++ PHP7.4/PHP-74.sh
@@ -6,12 +6,26 @@ ip="$3"
 home_dir="$4"
 docroot="$5"
 
+#!/bin/bash
+# Adding php pool conf
+user="$1"
+domain="$2"
+ip="$3"
+home_dir="$4"
+docroot="$5"
+
 a2dismod php5.6
+
 a2dismod php7.1
+
 a2dismod php7.2
+
 a2dismod php7.3
+
 a2dismod php7.4
+
 a2enmod php7.4
-service apache2 restart
+
+systemctl restart apache2
 
 exit 0
